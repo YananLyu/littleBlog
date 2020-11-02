6d73bd02c7b40577c4a85cba18b9699f2ff053cc
lyu <yanan.lyu@gmail.com>
2020-11-01 23:18:22 -0500

    store the blogs to MongoDB Atlas

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,12 +17,27 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let posts = [];
+mongoose.connect("mongodb+srv://admin-yanan:Admin-yanan@cluster0.wolfm.mongodb.net/blogDB?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+const blogSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+const Blog = mongoose.model('blog', blogSchema);
+
 
 app.get("/", function (req, res) {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+  db.on('error', console.error.bind(console, 'connection error'));
+
+  Blog.find(function (err, posts) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+      });
+    }
   });
 });
 
@@ -43,7 +59,17 @@ app.post("/compose", function (req, res) {
     content: req.body.postBody
   }
 
-  posts.push(post);
+  console.log(post);
+  db.on('error', console.error.bind(console, 'connection error'));
+
+  const blog = new Blog({
+    title: post.title,
+    content: post.content
+  });
+  blog.save(function (err) {
+    if (err) return handleError(err);
+    console.log("new blog is saved");
+  });
 
   res.redirect("/");
 });
@@ -51,19 +77,28 @@ app.post("/compose", function (req, res) {
 app.get("/posts/:postName", function (req, res) {
   const reqTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === reqTitle) {
-      console.log("Match Found!");
-      res.render("post",
-        {
-          title: post.title,
-          content: post.content
-        });
+  Blog.find(function (err, posts) {
+    if (err) {
+      console.log(err);
     } else {
-      console.log("Not a Match!");
+      posts.forEach(function (post) {
+        const storedTitle = _.lowerCase(post.title);
+        console.log(storedTitle, reqTitle);
+        if (storedTitle === reqTitle) {
+          console.log("Match Found!");
+          res.render("post",
+            {
+              title: post.title,
+              content: post.content
+            });
+        } else {
+          console.log("Not a Match!");
+        }
+      });
     }
   });
+
+
 })
 
 
